ad7b6e36ff9e416a507e986e2658cff1bffc9bfd
kyungs9364 <yks9364@naver.com>
2017-09-21 11:12:37 +0900

    boardview 추가 - 양경석

--- rnbcafe/src/main/java/com/kosta/rnbcafe/board/controller/BoardController.java
+++ rnbcafe/src/main/java/com/kosta/rnbcafe/board/controller/BoardController.java
@@ -2,6 +2,8 @@ package com.kosta.rnbcafe.board.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.kosta.rnbcafe.board.dto.BoardDto;
 import com.kosta.rnbcafe.board.service.BoardServiceImpl;
+import com.kosta.rnbcafe.member.dto.MemberDto;
 
 @Controller
 @RequestMapping(value="/board/")
@@ -34,13 +37,20 @@ public class BoardController {
 	
 	@RequestMapping(value="insertboard", method=RequestMethod.POST)
 	public String insertBoard(BoardDto dto) {
-		// img, id 값 준비해서 넣기
 		
 		int cnt = service.insertBoard(dto);
 		if(cnt == 0) {
 			return "board/insertboard";
 		}
 		return "redirect:/board/boardlist";
+	}
+	
+	@RequestMapping(value="boardview")
+	public String boradview(Model model, int bseq) {
+		BoardDto dto = service.boradview(bseq);
+		model.addAttribute("dto", dto);
+		
+		return "board/boardview";
 	}
 
 }
